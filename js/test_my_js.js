99aeb0111d279af1a4f759153daffa53a229fc24
Chiuin Meng <45228599+ChiuinMeng@users.noreply.github.com>
2019-06-18 13:49:54 +0800

    Site updated: 2019-06-18 13:49:51

--- js/test_my_js.js
+++ js/test_my_js.js
@@ -1,27 +1,12 @@
-console.log("检查是否有class为pdf的结点....");
+//console.log("检查是否有class为pdf的结点....");
 div_pdfs = document.getElementsByClassName("pdf");
-
-/*
-var loadingTask = pdfjsLib.getDocument(div_pdfs[0].getAttribute("target"));
-loadingTask.promise.then(function(pdf) {
-  // you can now use *pdf* here
-});
-
-pdf.getPage(1).then(function(page) {
-  // you can now use *page* here
-});
-
-var scale = 1.5;
-var viewport = page.getViewport({ scale: scale, });
-
-var canvas = document.getElementById('the-canvas');
-var context = canvas.getContext('2d');
-canvas.height = viewport.height;
-canvas.width = viewport.width;
-
-var renderContext = {
-  canvasContext: context,
-  viewport: viewport
-};
-page.render(renderContext);
-*/
+for(j = 0,len=div_pdfs.length; j < len; j++) {
+	var iframe = document.createElement('iframe');  
+	
+	iframe.name = 'ifr_pdf_show'+j;  
+	iframe.src="/pdf.js/web/viewer.html?file=" + document.URL + div_pdfs[j].getAttribute("target");
+	iframe.width=div_pdfs[0].offsetWidth;
+	iframe.height=560;
+	
+	div_pdfs[j].appendChild(iframe);
+}
